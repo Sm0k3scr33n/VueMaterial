68cb48c9ae334c5554f22537ab7295aa0b8ae49e
michael gabbard <sm0k3scr33n@michaels-MBP.hsd1.co.comcast.net>
2019-02-24 14:23:53 -0700

    :devil: first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,21 @@
+"use strict"
+
+Vue.use(VueMaterial.default);
+
+var navbar = {
+  name: "PersistentMini",
+  data: function data() {
+    return {
+      menuVisible: false
+    }
+  },
+  methods: {
+    toggleMenu: function toggleMenu() {
+      this.menuVisible = !this.menuVisible
+    }
+  }
+};
+
+var app = new Vue(navbar);
+
+app.$mount("#app")       ;
